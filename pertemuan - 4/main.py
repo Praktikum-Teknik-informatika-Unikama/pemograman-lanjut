0e49374111792f1b7913fd088ac3d7a51361e5cc
aufall02 <aufaluzumaki@gmail.com>
2024-05-14 08:18:08 +0700

    pertemuan 4

---
+++ pertemuan - 4/main.py
@@ -0,0 +1,35 @@
+class Kendaraan :
+    def __init__(self, nama, warna, harga):
+        self.nama = nama
+        self.warna = warna
+        self.harga = harga
+
+    def  tampilkan(self):
+        print('Details: {} {} {}'.format(self.nama, self.warna, self.harga))
+
+    def kecepatan_max(self):
+        print('kecepatan kendaraan max 150 km/jam')
+    
+    def change_gear(self):
+        print('kendaraan ini bisa berganti gear sampai 6')
+
+class Mobil(Kendaraan):
+
+    def kecepatan_max(self):
+        print('kecepatan kendaraan max 240 km/jam')
+    def change_gear(self):
+        print('kendaraan ini bisa berganti gear sampai 7')
+
+
+"UNTUK CLASS MOBIL"
+mobil = Mobil('toyota', 'ungu', 1000)
+mobil.tampilkan()
+mobil.kecepatan_max()
+mobil.change_gear()
+
+print("===================================")
+"UNTUK CLASS KENDARAAN"
+truck = Kendaraan('Truck', 'kuning', 2000)
+truck.tampilkan()
+truck.kecepatan_max()
+truck.change_gear()
