e89b17e98d6b34c5207d718b18661eb563e556c5
aufall02 <aufaluzumaki@gmail.com>
2024-05-28 07:30:57 +0700

    pertemuan 3

--- pertemuan - 3/main.py
+++ pertemuan - 3/main.py
@@ -1,39 +1,17 @@
-class Manusia:
-    nama = ''
-    umur = 0
-
-    def __init__(self,namaku,umurku):
+class Manusia: 
+    def __init__(self, namaku, umurku):
         self.nama = namaku
         self.umur = umurku
-        print('Object Manusia berhasil dibuat, dengan nama :', self.nama)
-
-    def get_nama(self):
-        print('nama saya adalah', self.nama)
-        return
+        self.pesan() 
+        
+    def pesan(self):
+        print('Object {} diciptakan .. ! dengan nama : {}'.format(type(self).__name__,self.nama))
 
 class Tentara(Manusia):
-    amu = 0
-    granat = 0
-    def __init__(self, namaku, umurku, jml_amunisi, jml_granat):
-        self.nama = namaku
-        self.umur = umurku
-        self.amu = jml_amunisi
-        self.granat = jml_granat
-        print('Object Tentara berhasil dibuat, dengan nama :', self.nama)
+    def __init__(self, namaku, umurku, ):
+        super().__init__(namaku, umurku)
 
 
-class Prajurit(Tentara):
-    pangkat = ''
-    def __init__(self, namaku, umurku, jml_amunisi, jml_granat, pangkatku):
-        self.nama = namaku
-        self.umur = umurku
-        self.amu = jml_amunisi
-        self.granat = jml_granat
-        self.pangkat = pangkatku
 
-        print('Object Prajurit berhasil dibuat, dengan nama :', self.nama, ' dan berpangkat', self.pangkat)
-
-febri = Tentara('febri', 20, 9, 2)
-udin = Prajurit('udin', 19, 4, 1, 'jendral')
-
-febri.get_nama()
+febri = Manusia('febri', 21)
+# print(type(febri).__name__)
---
+++ pertemuan - 3/tugas2.py
@@ -0,0 +1,6 @@
+"""
+- buatlah sebuah class parent atau induk dengan nama bebas, dan mempunyai minimal 2 atribut
+lalu buatlah function untuk menampilkan pesan yang menjelaskan bahwa object atau class berhasil dibuat dengan catatan pesan harus dinamis sesuai nama class
+
+- buatlah minimal 2 class child atau turunan sesuaikan dengan class induk (hierarki). tambahkan minimal 1 attribut disetiap class turunan dan buat juga satu fungsi tambahan. 
+"""
