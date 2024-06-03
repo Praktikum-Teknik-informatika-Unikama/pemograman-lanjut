c973b3989abac7096bff774fc445a62995f934fe
aufall02 <aufaluzumaki@gmail.com>
2024-06-04 06:07:17 +0700

    pertemuan 4 : update

--- pertemuan - 4/main.py
+++ pertemuan - 4/main.py
@@ -1,41 +1,84 @@
-class Kendaraan :
-    def __init__(self, nama, warna, harga):
+
+# class Kendaraan :
+#     def __init__(self, nama, warna, harga):
+#         self.nama = nama
+#         self.warna = warna
+#         self.harga = harga
+
+#     def  tampilkan(self):
+#         print('Details: {} {} {}'.format(self.nama, self.warna, self.harga))
+
+#     def kecepatan_max(self):
+#         pass
+    
+#     def change_gear(self):
+#         pass
+
+# class Mobil(Kendaraan):
+
+#     def kecepatan_max(self):
+#         print('kecepatan kendaraan max 240 km/jam')
+#     def change_gear(self):
+#         print('kendaraan ini bisa berganti gear sampai 7')
+
+# class Truck(Kendaraan):
+
+#     def kecepatan_max(self):
+#         print('kecepatan kendaraan max 160 km/jam')
+#     def change_gear(self):
+#         print('kendaraan ini bisa berganti gear sampai 10')
+
+# "UNTUK CLASS MOBIL"
+# mobil = Mobil('toyota', 'ungu', 1000)
+# mobil.tampilkan()
+# mobil.kecepatan_max()
+# mobil.change_gear()
+
+# print("===================================")
+# "UNTUK CLASS KENDARAAN"
+# truck = Truck('Truck', 'kuning', 2000)
+# truck.tampilkan()
+# truck.kecepatan_max()
+# truck.change_gear()
+
+
+
+
+
+
+
+
+class Kendaraan():
+    def __init__(self, nama,warna,harga):
         self.nama = nama
         self.warna = warna
         self.harga = harga
-
-    def  tampilkan(self):
-        print('Details: {} {} {}'.format(self.nama, self.warna, self.harga))
+    
+    def tampilkan(self):
+        print('Details :', self.nama, self.warna, self.harga)
 
     def kecepatan_max(self):
-        pass
+        print('kecepatan kendaraan max = 150 km/jam')
     
     def change_gear(self):
-        pass
+        print('kendaraan ini bisa berganti gear sampai 6')
 
 class Mobil(Kendaraan):
-
     def kecepatan_max(self):
-        print('kecepatan kendaraan max 240 km/jam')
+        print('kecepatan kendaraan max = 270 km/jam')
+
     def change_gear(self):
         print('kendaraan ini bisa berganti gear sampai 7')
 
-class Truck(Kendaraan):
 
-    def kecepatan_max(self):
-        print('kecepatan kendaraan max 160 km/jam')
-    def change_gear(self):
-        print('kendaraan ini bisa berganti gear sampai 10')
-
-"UNTUK CLASS MOBIL"
-mobil = Mobil('toyota', 'ungu', 1000)
+mobil = Mobil('mobil X', 'merah', 20000)
 mobil.tampilkan()
 mobil.kecepatan_max()
 mobil.change_gear()
 
-print("===================================")
-"UNTUK CLASS KENDARAAN"
-truck = Truck('Truck', 'kuning', 2000)
-truck.tampilkan()
-truck.kecepatan_max()
-truck.change_gear()
+
+kendaraan = Kendaraan('Truck X', 'kuning', 75000)
+
+kendaraan.tampilkan()
+kendaraan.kecepatan_max()
+kendaraan.change_gear()
--- pertemuan - 4/tugas.md
+++ pertemuan - 4/tugas.md
@@ -1,4 +1,4 @@
-# Tugas 
+# Tugas 1
 
 - Buatlah kelas `Pembayaran` dengan metode `proses_pembayaran`(self, jumlah) yang tidak melakukan apa-apa (pass).
 - Buat kelas `KartuKredit` yang merupakan turunan dari Pembayaran.
@@ -10,3 +10,20 @@
   - Override metode `proses_pembayaran`(self, jumlah) untuk mencetak pesan bahwa pembayaran sedang diproses menggunakan PayPal.
 
 - Buat variable untuk mamenggil object `KartuKredit` dan `PayPal`, kemudian panggil metode `proses_pembayaran` pada masing-masing objek.
+
+
+# Tugas
+
+- Buat kelas abstrak `LibraryItem` dengan metode abstrak `display_info` dan `calculate_late_fee`.
+- Buat kelas turunan `Book`, dan `DVD` yang mengimplementasikan metode dari kelas abstrak `LibraryItem`.
+- Implementasikan metode `display_info` untuk menampilkan informasi detail tentang item.
+- Implementasikan metode `calculate_late_fee` untuk menghitung biaya keterlambatan berdasarkan jumlah hari keterlambatan dan tarif per hari yang berbeda untuk setiap jenis item.
+
+## Book:
+Memiliki atribut `title`, `author`, dan `daily_late_fee` (misalnya, 5000 per hari).
+Biaya keterlambatan dihitung sebagai jumlah hari keterlambatan dikalikan dengan `daily_late_fee`.
+
+
+## DVD:
+Memiliki atribut `title`, `director`, dan `daily_late_fee` (misalnya, 10000 per hari).
+Biaya keterlambatan dihitung sebagai jumlah hari keterlambatan dikalikan dengan `daily_late_fee`.
